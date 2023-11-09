278a220da6c52abcb8c4dbf61fe6aab6033b91a9
DhewGanz <147708876+DhewGanz@users.noreply.github.com>
2023-11-09 16:48:49 +0700

    Update login.js
    menambahkan Regular Expression

--- login.js
+++ login.js
@@ -14,4 +14,13 @@ function validateForm() {
           return false;
      }
      return true;
+     if (username.length < 6) {
+          errorDiv.textContent = "Username harus minimal 6 karakter.";
+          return false;
+     }
+
+     if (password.length < 8 || !(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/.test(password))) {
+          errorDiv.textContent = "Password harus minimal 8 karakter dan berisi kombinasi huruf, angka, dan karakter khusus.";
+          return false;
+     }
 }
